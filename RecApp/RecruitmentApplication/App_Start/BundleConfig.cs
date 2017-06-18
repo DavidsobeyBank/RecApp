7d5475f6b3cea702f9c56ec389e9a51132d369f0
DavidsobeyBank <David.Sobey@standardbank.co.za>
2017-06-18 13:08:10 +0200

    Added Material Design Interface

--- RecApp/RecruitmentApplication/App_Start/BundleConfig.cs
+++ RecApp/RecruitmentApplication/App_Start/BundleConfig.cs
@@ -22,10 +22,6 @@ namespace RecruitmentApplication
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/site.css"));
-
         }
     }
 }
