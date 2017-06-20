b69d80d789d2f613eb58ed756b5da6f5f64b1a98
DavidsobeyBank <David.Sobey@standardbank.co.za>
2017-06-20 23:17:46 +0200

    Created the Interview/StartInterview Page and Modfied the Nav Bar

--- RecApp/RecruitmentApplication/Models/Student.cs
+++ RecApp/RecruitmentApplication/Models/Student.cs
@@ -30,7 +30,13 @@ namespace RecruitmentApplication.Models
         public string StudentPhoto { get; set; }
         public string StudentBio { get; set; }
         public string StudentVideo { get; set; }
-    
+
+        public string StudentBirth()
+        {
+            return StudentDOB.ToString("D");
+        }
+        
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Interview> Interviews { get; set; }
     }
