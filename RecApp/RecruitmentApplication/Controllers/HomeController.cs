ed93be505b659df9ceeaf3cf4f1f38f34f40f5c1
Sachin <sachinnaidoo@hotmail.com>
2017-07-07 10:28:08 +0200

    Reset password and some small ui changes to interview and login

--- RecApp/RecruitmentApplication/Controllers/HomeController.cs
+++ RecApp/RecruitmentApplication/Controllers/HomeController.cs
@@ -30,10 +30,12 @@ namespace RecruitmentApplication.Controllers
         [HttpPost]
         public ActionResult Index(string eMail, string pass)
         {
+            if(Request["Email"] == null || Request["Password"] == null)
+            {
+                return View();
+            }
             string email = Request["Email"].ToString();
-
-            //hardcoded
-            string password = "guest";
+            string password = Request["Password"].ToString();
 
             //check if user exists and password matches
             var user = db.Employees.FirstOrDefault(e => e.EmployeeEmail == email);
@@ -59,6 +61,68 @@ namespace RecruitmentApplication.Controllers
         {
             Session["userLoggedIn"] = null;
             return Redirect("~/Home/Index");
+        }
+
+        [HttpGet]
+        public ActionResult ResetPassword()
+        {
+            ViewBag.message = ""; 
+            return View(); 
+        }
+
+        [HttpPost]
+        public ActionResult ResetPassword(string oldPassword, string newPassword, string email)
+        {
+            //check if old password matches
+            var user = db.Employees.FirstOrDefault(e => e.EmployeeEmail == email);
+            if (user != null)
+            {
+                ICryptoService cryptoService = new PBKDF2();
+
+                string hashed = cryptoService.Compute(oldPassword, user.Salt);
+
+                if (hashed == user.Pass) //if the old password is correct
+                {
+                    //generate a new salt
+                    string salt = cryptoService.GenerateSalt();
+                    user.Salt = salt;
+
+                    //hash new password
+                    string hashedPassword = cryptoService.Compute(newPassword);
+                    user.Pass = hashedPassword;
+
+                    db.SaveChanges();
+                    //log the user in
+                    Session["userLoggedIn"] = user.EmployeeEmail;
+                    return RedirectToAction("Index", "Interviews");
+
+                }
+                else
+                {
+                    ViewBag.message = "Old password is incorrect, please try again";
+                    return View();
+                }
+            }
+            else
+            {
+                ViewBag.message = "No user with that email address exists, please try again";
+                return View();            
+            }
+
+
+        }
+
+        [HttpPost]
+        public ActionResult ForgotPassword(string email)
+        {
+            //check if user with that email exists
+            var user = db.Employees.FirstOrDefault(e => e.EmployeeEmail == email);
+            if (user != null)
+            {
+
+            }
+                //send an email to that user with their password.  (build smtp function)
+                return View();
         }
 
         #endregion
